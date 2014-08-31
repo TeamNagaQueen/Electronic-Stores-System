b20e8187774af42eaee1fe0fddebaa8cf8d16063
BorislavIvanov <borislav.a.ivanov@gmail.com>
2014-08-31 21:55:31 +0300

    New Version of SQLiteBD.Data was added

---
+++ SQLiteBD.Data/SQLiteDB.Data/TEST.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteDB.Data
+{
+    class TEST
+    {
+        public static void Main()
+        {
+            SQLiteDBTaxesEntities db = new SQLiteDBTaxesEntities();
+
+            using (db)
+            {
+                var allASD =
+                    from a in db.Products
+                    select a;
+
+                Console.WriteLine(allASD.Count());
+            }
+        }
+    }
+}
